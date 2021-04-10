64c966623b00e2b92798720c3ce81f48a6c64e39
DanielThorson <dragonflydan@gmail.com>
2021-04-10 12:15:37 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,82 @@
+class donutShop {
+  inventory;
+  revenue;
+  order;
+  
+  
+	constructor(inventory, revenue, order) {
+    this.inventory = inventory;
+    this.revenue = revenue;
+    this.order = order;
+  }
+	
+	addDonutsByType(donutType, donutCount){
+		this.inventory = this.inventory.map(function(donut){
+			if(donutType === donut.type){
+				donut.count += donutCount;
+			}
+			return donut;
+		});
+	}
+}
+
+class Donut {
+  type;
+  amount;
+  price;
+  
+  
+	constructor(type, count, price) {
+    this.type = type;
+    this.count = count;
+    this.price = price;
+  }
+}
+
+let DonutShop = new donutShop([new Donut('creme filled', 3, 2), new Donut('plain', 2, 1)], '', 'maple bacon','eggs',['']);
+
+let b = new Donut([{type: 'creme filled', count: 2, price: 2}, {type: 'plain', count: 1, price: 1}]);
+
+
+var input;
+// inventory needs to be a class with in that has donut name and price
+
+while(input !== '6'){
+  input = prompt( `Please make a selection:
+1. Print donut inventory / donut prices
+2. Print revenue
+3. Create new donut type
+4. Add donuts to Inventory
+5. Place customer order
+6. Quit` );
+  if (input === "1"){
+  console.log ( `This is the inventory:
+  `);
+
+	DonutShop.inventory.forEach(function(donut){
+    console.log(`type: ${donut.type}`);
+    console.log(`price: ${donut.price.toLocaleString('en-us',{ style: 'currency', currency: 'usd'})}`);
+    console.log(`count: ${donut.count}`);
+  }); 
+  // console.log(a.inventory);
+} else if (input === "2"){
+  alert ( `Total Shop Revenue ${a.revenue}` );
+} else if (input === "3"){
+  let type = prompt( `What type of donut is it?` );
+  let price = prompt( `How much does it cost?` );
+  
+	DonutShop.inventory.push(new Donut(type, 0, price));
+}else if (input === "4"){
+  let type = prompt ( `What type of donut do you want to add more of?	 
+		${DonutShop.inventory}` );  // grabs from list of inventory
+  let number = prompt ( `How many do you want to add?` ); // adds to order
+	
+	DonutShop.addDonutsByType(type, parseInt(number))
+}else if (input === "5"){
+  a.order.push(prompt( `What type of donut does customer want?
+  ${DonutShop.inventory}` ));
+  DonutShop.order.push(prompt( `How many do they want?` ));
+  alert ( DonutShop.order);
+}
+}
+
